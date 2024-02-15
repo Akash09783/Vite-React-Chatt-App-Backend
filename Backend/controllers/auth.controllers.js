3a7642f850052393cf89496ca66234629adcd1d5
Akash <kumawat.aakash515@gmail.com>
2024-02-15 14:38:57 +0530

    Api Routes setup done

---
+++ Backend/controllers/auth.controllers.js
@@ -0,0 +1,13 @@
+export const signup = (req, res) => {
+    res.send("signupUser")
+  console.log("signup User");
+};
+export const login = (req, res) => {
+    res.send("loginUser")
+  console.log("login User");
+};
+
+export const logout = (req, res) => {
+    res.send("logoutUser")
+  console.log("logout User");
+};
