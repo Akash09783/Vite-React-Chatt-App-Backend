d59366a45a2e9c1329fd9ca7bf848ffd28ee6cc4
Akash <kumawat.aakash515@gmail.com>
2024-02-16 21:09:58 +0530

    changes made

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { configDotenv } from "dotenv";
 import connectToMongoDb from "./db/connectToMongoDb.js";
-const app = express();
+import {app,server} from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDb();
   console.log(`server running on port ${PORT}`);
 });
